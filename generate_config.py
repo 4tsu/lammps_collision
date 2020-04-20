bbcbd7a837ba2bf0f0ca4f43c24840b934b7dbfa
S. Yotsuji <shoyocho0116@gmail.com>
2020-04-20 16:09:41 +0900

    different radius generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -9,8 +9,8 @@ class Atom:
         self.vz = 0.0
 
 
-def add_ball(atoms, xpos, xvel):
-    r = 8
+def add_ball(atoms, xpos, xvel, radius):
+    r = radius
     s = 1.55
     h = 0.5 * s
     for ix in range(-r, r+1):
@@ -49,7 +49,7 @@ def save_file(filename, atoms):
 
 atoms = []
 
-add_ball(atoms, -20, 10.0)
-add_ball(atoms, 20, -10.0)
+add_ball(atoms, -20, 10.0, 10)
+add_ball(atoms, 20, -10.0, 5)
 
 save_file("collision.atoms", atoms)
