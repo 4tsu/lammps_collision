3d47ea264db4346b799f7fe8ead47360b2a9933e
S. Yotsuji <shoyocho0116@gmail.com>
2020-05-04 17:26:57 +0900

    one atom version

--- generate_config.py
+++ generate_config.py
@@ -11,7 +11,7 @@ class Atom:
 
 def add_ball(atoms, xpos, xvel):
     r = 8
-    s = 1.55
+    s = 1.55                                        # 原子間距離のルート2倍
     h = 0.5 * s
     for ix in range(-r, r+1):
         for iy in range(-r, r+1):
@@ -19,14 +19,19 @@ def add_ball(atoms, xpos, xvel):
                 x = ix * s
                 y = iy * s
                 z = iz * s
-                if (x**2 + y**2 + z**2 > r**2):
+                if (x**2 + y**2 + z**2 > r**2):     # 原子を球状に配置するため
                     continue
-                x = x + xpos
-                atoms.append(Atom(x, y, z, xvel))
+                x = x + xpos                        # 空間中に原子を配置
+                atoms.append(Atom(x, y, z, xvel))   # 斜め方向に原子を配置
                 atoms.append(Atom(x, y+h, z+h, xvel))
                 atoms.append(Atom(x+h, y, z+h, xvel))
                 atoms.append(Atom(x+h, y+h, z, xvel))
 
+def add_one_ball(atoms, xpos, xvel):
+    y = 0
+    z = 0
+    x = xpos
+    atoms.append(Atom(x, y, z, xvel))
 
 def save_file(filename, atoms):
     with open(filename, "w") as f:
@@ -49,7 +54,7 @@ def save_file(filename, atoms):
 
 atoms = []
 
-add_ball(atoms, -20, 2.0)
-add_ball(atoms, 20, -2.0)
+add_one_ball(atoms, -10, 1.0)
+add_one_ball(atoms, 10, -1.0)
 
 save_file("collision.atoms", atoms)
